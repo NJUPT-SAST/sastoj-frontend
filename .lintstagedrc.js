e118e5059374389e745606a613bbf18d7a148bcb
MaxtuneLee <max@xox.im>
2023-12-20 22:25:41 +0800

    build: fix lintstage

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,14 +1,14 @@
 export default {
-    "packages/competition/*.{js,cjs,ts,json,css,scss,tsx,sass}": () => [
+    "packages/competition/**/*.{js,cjs,ts,json,css,scss,tsx,sass}": () => [
         "pnpm --filter @sast/oj-competition lint",
         "pnpm --filter @sast/oj-competition build",
         "git add ."
     ],
-    "packages/docs/*.{js,cjs,ts,html,json,css,scss,tsx,sass,md,mdx}": () => [
+    "packages/docs/**/*.{js,cjs,ts,html,json,css,scss,tsx,sass,md,mdx}": () => [
         "pnpm --filter @sast/oj-docs build",
         "git add ."
     ],
-    "packages/ui/*.{js,cjs,ts,html,json,css,scss,tsx,sass}": () => [
+    "packages/ui/**/*.{js,cjs,ts,html,json,css,scss,tsx,sass}": () => [
         "pnpm --filter @sast/oj-ui test",
         "pnpm --filter @sast/oj-ui format",
         "pnpm --filter @sast/oj-ui lint",
