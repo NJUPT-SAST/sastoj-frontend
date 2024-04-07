2a471c5170c8b9abd795bbd0bf79363dffe66b18
BQX <b22050106@njupt.edu.cn>
2024-04-07 19:17:03 +0800

    delete(ui): 删除本地ui组件

--- packages/competition/src/pages/login/page.tsx
+++ packages/competition/src/pages/login/page.tsx
@@ -1,30 +1,13 @@
 import LoginCard from "../../components/login/loginCard";
 import styles from "./page.module.scss";
-import { Button } from "@sast/oj-ui-universal";
 
 const Login = () => {
   return (
-    <>
-      <div className={styles["login-background"]}>
-        <div className={styles["login-card-container"]}>
-          <LoginCard></LoginCard>
-          <Button
-            _onclick={() => {
-              console.log("click");
-            }}
-          >
-            Click me
-          </Button>
-          <button
-            onClick={() => {
-              console.log("click");
-            }}
-          >
-            click
-          </button>
-        </div>
+    <div className={styles["login-background"]}>
+      <div className={styles["login-card-container"]}>
+        <LoginCard></LoginCard>
       </div>
-    </>
+    </div>
   );
 };
 
