e60cefe489377bd44340be07f88a8209a6fb21cd
BQX <b22050106@njupt.edu.cn>
2024-04-08 12:40:21 +0800

    feat(web): axios的二次封装

--- packages/competition/src/utils/web/request.ts
+++ packages/competition/src/utils/web/request.ts
@@ -1,13 +1,19 @@
 import axios from "axios";
 
-const BASE_URL = "/test";
+// axios二次封装
+const devBaseURL = "在开发环境下接口的baseURL";
+const proBaseURL = "在生产环境下接口的baseURL";
+//在vue的config文件夹下可以看开发环境和生产环境的名称,通常为development和production
+const BASE_URL =
+  process.env.NODE_ENV === "development" ? devBaseURL : proBaseURL;
+
 const TIMEOUT_MILLISECONDS = 60000; // 超时链接
 
-const instance = axios.create({
+const service = axios.create({
   //   生产环境中禁止跨域
   //   withCredentials: false,
   baseURL: BASE_URL,
   timeout: TIMEOUT_MILLISECONDS,
 });
 
-export default instance;
+export default service;
