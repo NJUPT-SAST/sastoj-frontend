5f5db45511b6aa5efc1d0e35ad143c80136a33a5
BQX <b22050106@njupt.edu.cn>
2024-04-10 14:02:12 +0800

    feat(markdown): 完善 useMarkdown Hook #2

--- packages/competition/src/pages/home/page.tsx
+++ packages/competition/src/pages/home/page.tsx
@@ -4,24 +4,15 @@ import Logo from "../../components/logo";
 import { Home as HomeIcon, LayoutList, Award } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { convertMarkdownToHtml } from "../../utils/markdown";
+import useMarkdown from "../../hooks/useMarkdown";
+
+const markdown =
+  '# Markdown Sample\n\nThis is a **Markdown** sample with various elements.\n\n## Lists\n- [x] Task 1\n- [ ] Task 2\n- Item 3\n1. First item\n2. Second item\n3. Third item like this source code\n\n## Code Block\n\n```javascript\nfunction greet(name) {\n  console.log("Hello, "!");\n}\n```\n\n## Links\n\n[OpenAI](https://openai.com)\n\n## Table\n\n| Name  | Age | Gender |\n|-------|-----|--------|\n| John  | 25  | Male   |\n| Emily | 30  | Female |\n| Mark  | 28  | Male   |\n\n## Blockquote\n\n> Markdown is a lightweight markup language used for formatting text. It is widely used for creating documentation, README files, and web content.\n\nThat\'s it for the Markdown sample. Enjoy!';
 
 const Home = () => {
   const [selectedRoute, setSelectedRoute] = useState<string>("");
-  const [markdownToHtml, setMarkdownToHtml] = useState<string>("");
   const navigate = useNavigate();
-
-  useEffect(() => {
-    convertMarkdownToHtml(
-      '# Markdown Sample\n\nThis is a **Markdown** sample with various elements.\n\n## Lists\n\n1. First item\n2. Second item\n3. Third item like this source code\n\n## Code Block\n\n```javascript\nfunction greet(name) {\n  console.log("Hello, "!");\n}\n```\n\n## Links\n\n[OpenAI](https://openai.com)\n\n## Table\n\n| Name  | Age | Gender |\n|-------|-----|--------|\n| John  | 25  | Male   |\n| Emily | 30  | Female |\n| Mark  | 28  | Male   |\n\n## Blockquote\n\n> Markdown is a lightweight markup language used for formatting text. It is widely used for creating documentation, README files, and web content.\n\nThat\'s it for the Markdown sample. Enjoy!'
-    )
-      .then((html: string) => {
-        setMarkdownToHtml(html);
-      })
-      .catch((error) => {
-        console.log("Convert markdown to html error:", error);
-      });
-  }, []);
+  const html = useMarkdown(markdown);
 
   const headerItems: NavbarItemProps[] = [
     {
@@ -55,7 +46,7 @@ const Home = () => {
         onchange={(value: string) => setSelectedRoute(value)}
       />
       <div
-        dangerouslySetInnerHTML={{ __html: markdownToHtml }}
+        dangerouslySetInnerHTML={{ __html: html }}
         style={{ width: "50%", margin: "10px" }}
       />
       This is Home
