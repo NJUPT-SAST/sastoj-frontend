b86e1eae75552c377ff73bdafb6bfb77848a549b
BQX <b22050106@njupt.edu.cn>
2024-04-15 21:10:41 +0800

    feat(login): 完成login页面

--- packages/competition/src/utils/web/request.ts
+++ packages/competition/src/utils/web/request.ts
@@ -9,11 +9,11 @@ const BASE_URL =
 
 const TIMEOUT_MILLISECONDS = 60000; // 超时链接
 
-const service = axios.create({
+const REQUEST = axios.create({
   //   生产环境中禁止跨域
   //   withCredentials: false,
   baseURL: BASE_URL,
   timeout: TIMEOUT_MILLISECONDS,
 });
 
-export default service;
+export default REQUEST;
