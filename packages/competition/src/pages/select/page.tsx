374c46e2c47b78e0407651f3192265e7c6d63444
2782522198@qq.com <2782522198@qq.com>
2024-04-22 22:20:41 +0800

    styles:select页面

--- packages/competition/src/pages/select/page.tsx
+++ packages/competition/src/pages/select/page.tsx
@@ -1,5 +1,84 @@
+import { useEffect } from "react";
+import SelectLayout from "./layout";
+import styles from './page.module.scss'
+import { Button, Card } from "@ui-aurora/react";
+
 const Select = () => {
-  return <>This is Select</>;
+  useEffect(() => {
+    //get compitions
+    console.log("比赛列表");
+
+  }, [])
+  return (
+    <SelectLayout>
+      <div className={styles['select-boxs']}>
+        <Card
+          padding={20} className={styles['select-item']} header={null} footer={null}
+          mainContent={<>
+            <div className={styles['item-title']}>
+              <div>SAST</div>
+              <div className={styles['competition-title']}>SAST OJ</div>
+            </div>
+            <div className={styles['item-content']}>
+              THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ .
+            </div>
+            <div className={styles['btn-box']}>
+              <Button className={styles['enter-btn']} size="small" color="secondary">ENTER</Button>
+            </div>
+          </>}
+        >
+        </Card>
+        <Card
+          padding={20} className={styles['select-item']} header={null} footer={null}
+          mainContent={<>
+            <div className={styles['item-title']}>
+              <div>SAST</div>
+              <div className={styles['competition-title']}>SAST OJ</div>
+            </div>
+            <div className={styles['item-content']}>
+              THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ .
+            </div>
+            <div className={styles['btn-box']}>
+              <Button className={styles['enter-btn']} size="small" color="secondary">ENTER</Button>
+            </div>
+          </>}
+        >
+        </Card>
+        <Card
+          padding={20} className={styles['select-item']} header={null} footer={null}
+          mainContent={<>
+            <div className={styles['item-title']}>
+              <div>SAST</div>
+              <div className={styles['competition-title']}>SAST OJ</div>
+            </div>
+            <div className={styles['item-content']}>
+              THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ .
+            </div>
+            <div className={styles['btn-box']}>
+              <Button className={styles['enter-btn']} size="small" color="secondary">ENTER</Button>
+            </div>
+          </>}
+        >
+        </Card>
+        <Card
+          padding={20} className={styles['select-item']} header={null} footer={null}
+          mainContent={<>
+            <div className={styles['item-title']}>
+              <div>SAST</div>
+              <div className={styles['competition-title']}>SAST OJ</div>
+            </div>
+            <div className={styles['item-content']}>
+              THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ .
+            </div>
+            <div className={styles['btn-box']}>
+              <Button className={styles['enter-btn']} size="small" color="secondary">ENTER</Button>
+            </div>
+          </>}
+        >
+        </Card>
+      </div>
+    </SelectLayout>
+  );
 };
 
 export default Select;
