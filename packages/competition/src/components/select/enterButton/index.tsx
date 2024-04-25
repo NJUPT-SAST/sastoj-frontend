3c4dca118be92a8356420771f9a19b57cf70fe4d
bqxbqx <boqingxin14@gmail.com>
2024-04-25 11:13:40 +0800

    style(select): 样式优化

--- packages/competition/src/components/select/enterButton/index.tsx
+++ packages/competition/src/components/select/enterButton/index.tsx
@@ -1,87 +1,82 @@
-import { Button, Dialog } from "@ui-aurora/react";
-import { useNavigate } from "react-router-dom";
 import React from "react";
+import { Button, Checkbox, Dialog, showToast } from "@ui-aurora/react";
+import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 import styles from "./index.module.scss";
 
 interface EnterButtonProps {
-  competionId: number;
+  competitionId: number;
 }
 
-const EnterButton: React.FC<EnterButtonProps> = ({ competionId }) => {
+const EnterButton: React.FC<EnterButtonProps> = ({ competitionId }) => {
   const [visible, setVisible] = useState<boolean>(false);
+  const [isChecked, setIsChecked] = useState<boolean>(false);
   const navigate = useNavigate();
 
-  const showDialog = () => {
-    setVisible(true);
-    console.log("打开考试规范遵守");
-  };
-
   const handleOk = () => {
-    setVisible(!visible);
-    navigate("/About");
-    localStorage.setItem("id", competionId.toString());
+    if (!isChecked) {
+      showToast({
+        type: "error",
+        content: <span>请勾选考试条约</span>,
+      });
+    } else {
+      navigate("/About");
+      localStorage.setItem("competitionId", competitionId.toString());
+    }
   };
 
   return (
-    <div className={styles["btn-box"]}>
-      <Button
-        onClick={showDialog}
-        className={styles["enter-btn"]}
-        size="small"
-        color="secondary"
-      >
-        进入比赛
-      </Button>
+    <>
+      <div className={styles["btn-box"]}>
+        <Button onClick={() => setVisible(true)} size="small" color="secondary">
+          进入比赛
+        </Button>
+      </div>
       <Dialog
+        className={styles.dialog}
         visible={visible}
         onOk={handleOk}
-        onCancel={() => {
-          setVisible(false);
-        }}
-        // className={styles['confirm-box']}
-        size="small"
+        onCancel={() => setVisible(false)}
+        size="medium"
         color="secondary"
-        header={
-          <div>
-            <p style={{ fontSize: "18px", fontWeight: "bold" }}>SAST比赛准则</p>
-          </div>
-        }
+        header={<span className={styles["dialog-title"]}>比赛准则</span>}
         mainContent={
-          <div className={styles["main-content"]}>
-            <p
-              style={{ lineHeight: 1.8, fontWeight: "650", textIndent: "2em" }}
-            >
-              欢迎你参加本次SAST比赛！
-              在参加考试前，考生请务必查阅具体的考试规则和要求，以确保您能准确了解和遵守相关规定。在确认同意后方可进入比赛，祝您考试顺利！
-            </p>
+          <div className={styles["dialog-content"]}>
+            <span>
+              欢迎你参加本次比赛！在参加考试前，考生请务必查阅具体的考试规则和要求，以确保您能准确了解和遵守相关规定。在确认同意后方可进入比赛，祝您考试顺利！
+            </span>
             <ul>
               <li>
-                <p>考生仅允许携带A4草稿纸和笔。</p>
+                <span>考生仅允许携带A4草稿纸和笔。</span>
               </li>
               <li>
-                <p>考试开始前，请将手机关机并上交给监考讲师</p>
+                <span>考试开始前，请将手机关机并上交给监考讲师</span>
               </li>
               <li>
-                <p>考试开始后，考生不能离开考场，除非经允许。</p>
+                <span>考试开始后，考生不能离开考场，除非经允许。</span>
               </li>
               <li>
-                <p>考试中，考生不能使用互联网或任何在线资源。</p>
-              </li>
-              <li>
-                <p>
-                  考试中，考生不能与其他考生交谈或沟通，包括口头、书面或电子方式
-                </p>
-              </li>
-              <li>
-                <p>考试中，考生不能抄袭、作弊或从其他来源获取非法帮助。</p>
+                <span>考试中，考生不能使用互联网或任何在线资源。</span>
               </li>
             </ul>
           </div>
         }
+        footer={
+          <div className={styles["dialog-footer"]}>
+            <Checkbox
+              value="checked"
+              label="我已阅读准则并严格遵守"
+              defaultChecked={isChecked}
+              onChecked={(value) => {
+                value === "add" ? setIsChecked(true) : setIsChecked(false);
+              }}
+            />
+            <Button onClick={handleOk}>进入比赛</Button>
+          </div>
+        }
       />
-    </div>
+    </>
   );
 };
 
--- packages/competition/src/pages/select/page.tsx
+++ packages/competition/src/pages/select/page.tsx
@@ -1,91 +1,45 @@
-import { useEffect } from "react";
+import { Button, Card } from "@ui-aurora/react";
+
 import SelectLayout from "./layout";
-import { useNavigate } from "react-router-dom";
+import styles from "./page.module.scss";
 import EnterButton from "../../components/select/enterButton";
 
-import styles from "./page.module.scss";
-import { Button, Card } from "@ui-aurora/react";
-import { Dialog } from "@ui-aurora/react";
-
 const Select = () => {
-  const navigate = useNavigate();
-  useEffect(() => {
-    //get compitions
-    console.log("比赛列表");
-  }, []);
   return (
     <SelectLayout>
-      <div className={styles["select-boxs"]}>
-        <Dialog>Enter Botton</Dialog>
+      <div className={styles["select-boxes"]}>
         <Card
-          padding={20}
-          className={styles["select-item"]}
-          header={null}
-          footer={null}
-          mainContent={
-            <>
-              <div className={styles["item-title"]}>
-                <div>SAST</div>
-                <div className={styles["competition-title"]}>SAST OJ</div>
-              </div>
-              <div className={styles["item-content"]}>
-                THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE
-                SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ .
-                THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE
-                SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ .
-                THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE
-                SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ .
-                THIS IS THE SAST OF OJ .
-              </div>
-              <div className={styles["btn-box"]}>
-                <Button
-                  onClick={() => {
-                    navigate("/Competition/id=1");
-                  }}
-                  className={styles["enter-btn"]}
-                  size="small"
-                  color="secondary"
-                >
-                  ENTER
-                </Button>
-              </div>
-            </>
-          }
-        ></Card>
-
-        <Card
-          padding={20}
-          className={styles["select-item"]}
-          header={null}
-          footer={null}
-          mainContent={
-            <>
-              <div className={styles["item-title"]}>
-                <div>SAST</div>
-                <div className={styles["competition-title"]}>SAST OJ</div>
-              </div>
-              <div className={styles["item-content"]}>
-                THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE
-                SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ .
-                THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE
-                SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ .
-                THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE
-                SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ .
-                THIS IS THE SAST OF OJ .
-              </div>
-              <EnterButton competionId={1} />
-            </>
-          }
-        ></Card>
-
-        <Card
-          padding={12}
+          padding={15}
           className={styles["select-item"]}
           header={
             <div className={styles["item-title"]}>
-              <span>SAST</span>
+              <span>创意艺术协会</span>
               <div className={styles["competition-title"]}>
-                <span>SAST OJ</span>
+                <span>创意设计大赛</span>
+              </div>
+            </div>
+          }
+          footer={
+            <div className={styles["btn-box"]}>
+              <EnterButton competitionId={1} />
+            </div>
+          }
+          mainContent={
+            <div className={styles["item-content"]}>
+              <span>
+                创意设计大赛是一个面向创意设计领域的比赛活动。参赛者可以展示自己的设计才华，创造出独特而有创意的作品。比赛涵盖平面设计、插图、动画、产品设计等多个类别。我们鼓励参赛者展现个人风格和创新思维，同时提供了宝贵的机会与其他设计师交流和学习。
+              </span>
+            </div>
+          }
+        ></Card>
+        <Card
+          padding={15}
+          className={styles["select-item"]}
+          header={
+            <div className={styles["item-title"]}>
+              <span>编程之友社区</span>
+              <div className={styles["competition-title"]}>
+                <span>编程挑战赛</span>
               </div>
             </div>
           }
@@ -103,29 +57,67 @@ const Select = () => {
           mainContent={
             <div className={styles["item-content"]}>
               <span>
-                THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE
-                SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ .
-                THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE
-                SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ .
-                THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE
-                SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ .
-                THIS IS THE SAST OF OJ . IS THE SAST OF OJ . THIS IS THE SAST OF
-                OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS
-                THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ
-                . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE
-                SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ .
-                THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE
-                SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ .
-                THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE
-                SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ .
-                THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE
-                SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ .
-                THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE
-                SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ .
-                THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE
-                SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ .
-                THIS IS THE SAST OF OJ . THIS IS THE SAST OF OJ . THIS IS THE
-                SAST OF OJ . THIS
+                编程挑战赛是一个面向编程爱好者的比赛活动。参赛者将面临一系列有趣而富有挑战性的编程题目，涵盖算法、数据结构、编程逻辑等方面。比赛旨在提供一个展示编程技能和交流经验的平台。无论你是新手还是经验丰富的程序员，都欢迎加入这场激动人心的编程挑战赛！
+              </span>
+            </div>
+          }
+        ></Card>
+        <Card
+          padding={15}
+          className={styles["select-item"]}
+          header={
+            <div className={styles["item-title"]}>
+              <span>体育协会</span>
+              <div className={styles["competition-title"]}>
+                <span>体育竞技锦标赛</span>
+              </div>
+            </div>
+          }
+          footer={
+            <div className={styles["btn-box"]}>
+              <Button
+                className={styles["enter-btn"]}
+                color="secondary"
+                size="small"
+              >
+                进入比赛
+              </Button>
+            </div>
+          }
+          mainContent={
+            <div className={styles["item-content"]}>
+              <span>
+                体育竞技锦标赛是一个激烈而精彩的体育比赛活动。我们将举办多个项目，包括足球、篮球、田径、游泳等。无论你是热爱运动的业余爱好者还是专业运动员，都可以参加这场竞技锦标赛。比赛旨在促进体育运动的发展，提倡团队合作和健康生活方式。加入我们，展示你的体育才能，与其他运动爱好者一起竞技。
+              </span>
+            </div>
+          }
+        ></Card>
+        <Card
+          padding={15}
+          className={styles["select-item"]}
+          header={
+            <div className={styles["item-title"]}>
+              <span>编程之友社区</span>
+              <div className={styles["competition-title"]}>
+                <span>编程挑战赛</span>
+              </div>
+            </div>
+          }
+          footer={
+            <div className={styles["btn-box"]}>
+              <Button
+                className={styles["enter-btn"]}
+                color="secondary"
+                size="small"
+              >
+                进入比赛
+              </Button>
+            </div>
+          }
+          mainContent={
+            <div className={styles["item-content"]}>
+              <span>
+                编程挑战赛是一个面向编程爱好者的比赛活动。参赛者将面临一系列有趣而富有挑战性的编程题目，涵盖算法、数据结构、编程逻辑等方面。比赛旨在提供一个展示编程技能和交流经验的平台。无论你是新手还是经验丰富的程序员，都欢迎加入这场激动人心的编程挑战赛！
               </span>
             </div>
           }
