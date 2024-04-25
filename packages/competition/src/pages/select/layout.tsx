e0c293df4bbde39e9b46002168503183f92870e9
2782522198@qq.com <2782522198@qq.com>
2024-04-25 21:56:24 +0800

    feat: header Avatar头像

--- packages/competition/src/pages/select/layout.tsx
+++ packages/competition/src/pages/select/layout.tsx
@@ -3,6 +3,7 @@ import Footer from "../../components/footer";
 import Logo from "../../components/logo";
 import styles from "./page.module.scss";
 import { Navbar } from "@ui-aurora/react";
+import Avatar from "../../components/Avatar";
 
 interface SelectLayoutProps {
   children: ReactNode;
@@ -11,7 +12,11 @@ interface SelectLayoutProps {
 const SelectLayout: React.FC<SelectLayoutProps> = ({ children }) => {
   return (
     <div className={styles["select-page"]}>
-      <Navbar header={<Logo height={36} />} className={styles.navbar} />
+      <Navbar
+        header={<Logo height={36} />}
+        footer={<Avatar height={36} />}
+        className={styles.navbar}
+      />
       <div className={styles["select-content"]}>{children}</div>
       <Footer />
     </div>
