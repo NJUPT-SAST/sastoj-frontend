3523b0413c98bd0d5048d18dd90f89f699059be5
2782522198@qq.com <2782522198@qq.com>
2024-08-10 00:07:06 +0800

    fix: 历史提交新增条件判断渲染

--- packages/competition/src/components/problems/statusCardContent/evaluationRecord/index.tsx
+++ packages/competition/src/components/problems/statusCardContent/evaluationRecord/index.tsx
@@ -8,23 +8,21 @@ export const EvaluationRecord = () => {
   const contestId = localStorage.getItem("contestId");
   const { problemId } = useParams();
   const { data } = useSwrHistorySubmits(contestId as unknown as string, problemId as unknown as string)
-  console.log('<<<<<<<');
-  console.log(data);
 
 
   return (
     <div className={styles["evaluation-record-container"]}>
-      {/* {detail.map((item, index) => {
+      {data?.submissions ?data?.submissions.map((item, index) => {
         return (
           <div className={styles["evaluation-record-item"]} key={index}>
             <span>#id:{item.id}</span>
             <span>status:{item.status}</span>
             <span>point：{item.point}</span>
-            <span>time:{item.totalTime}</span>
-            <span>memory:{item.maxMemory}</span>
+            {/* <span>time:{item.totalTime}</span>
+            <span>memory:{item.maxMemory}</span> */}
           </div>
         );
-      })} */}
+      }): ''}
     </div>
   );
 };
