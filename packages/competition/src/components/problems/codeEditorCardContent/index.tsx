95e37f0a84f65b87fe72ae50560fa5122b086542
2782522198@qq.com <2782522198@qq.com>
2024-08-19 22:05:58 +0800

    fix: 修改语言类型和代码字体样式

--- packages/competition/src/components/problems/codeEditorCardContent/index.tsx
+++ packages/competition/src/components/problems/codeEditorCardContent/index.tsx
@@ -66,6 +66,36 @@ const optionsList: OptionProps[] = [
     value: "Bash",
     label: "Bash",
     key: 9,
+  },
+  {
+    label: 'Java',
+    value: 'Java',
+    key: 10
+  },
+  {
+    label: 'Golang',
+    value: 'Golang',
+    key: 11
+  },
+  {
+    label: 'NodeJS',
+    value: 'NodeJS',
+    key: 12
+  },
+  {
+    label: 'PHP',
+    value: 'PHP',
+    key: 13
+  },
+  {
+    label: 'Python3',
+    value: 'Python3',
+    key: 14
+  },
+  {
+    label: "Ruby",
+    value: 'Ruby',
+    key: 15
   }
 ];
 
